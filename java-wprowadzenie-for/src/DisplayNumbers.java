0bb2dd2c8c66300d5607f4c352bde2e8a61005a1
martincyrulik <martincyrulik@gmail.com>
2025-02-11 18:39:21 +0100

    #1 Wyświetlenie liczb od 91 do -7

---
+++ java-wprowadzenie-for/src/DisplayNumbers.java
@@ -0,0 +1,17 @@
+/**
+ * Wyświetl liczby od 91 do -7 korzystając z pętli for
+ */
+
+public class DisplayNumbers {
+    public static void main(String[] args) {
+
+//          zadanie 1 od 91 do -7
+//        System.out.println(91);
+//        System.out.println(90);
+//        System.out.println(89);
+
+        for (int i = 91; i >= -7; i--) {
+            System.out.println(i);
+        }
+    }
+}
